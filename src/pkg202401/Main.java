7d57c605e8e5c28125063c43df38854938e01290
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-22 15:01:07 +0530

    The Ultimate Data Structures & Algorithms - Part 1

--- src/pkg202401/Main.java
+++ src/pkg202401/Main.java
@@ -1,7 +1,12 @@
 package pkg202401;
 
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    int[] numbers = new int[3];
+    System.out.println(numbers);
+    System.out.println(Arrays.toString(numbers));
   }
 }
