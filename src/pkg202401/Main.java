d20dc4d6726b0f3d490b02db8f1ae118a7206d5b
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-01-22 16:03:48 +0530

    Update Main.java

--- src/pkg202401/Main.java
+++ src/pkg202401/Main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
-    int[] numbers = new int[3];
+    // int[] numbers = new int[3];
+    int[] numbers = { 10, 20, 30 };
     System.out.println(numbers);
     System.out.println(Arrays.toString(numbers));
   }
